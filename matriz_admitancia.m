5fa580c2e42b78d06d64a9af47a360105e4134d8
Thiago E. Kalid <thiagokalid@protonmail.com>
2020-03-12 17:48:23 -0300

    DSDS

--- matriz_admitancia.m
+++ matriz_admitancia.m
@@ -1,15 +1,17 @@
 clear 
 clc
-
 n = 3;
 Z_base = 1190.25;           % Unidade em OhmsPode ser extendido para formula
 Z_12_base = (5.55 + 56.4j); % Unidade em Ohms
 Z_13_base = (7.40 + 75.2j);
 Z_23_base = (5.55 + 56.4j);
 
-Z_12  = Z_12_base / Z_base  % Por unidade (pu)
-Z_13 = Z_13_base / Z_base
-Z_23 = Z_23_base / Z_base
+Z_12 = 0.0047 + j*0.0474
+Z_13 = 0.0062 + j*0.0632
+Z_23 = 0.0047 + j*0.0474
+
+B = [ (j*675e-6/2) , ((j*900e-6)/2) , ((j*675e-6)/2) ];
+
 
 matriz_Z = zeros(n,n);
 matriz_Z = [0 Z_12 Z_13 ; Z_12 0 Z_23 ; Z_13 Z_12 0]; % Impedancia das linhas de transmissao. Zero igual a nao existir linha.
@@ -18,16 +20,19 @@ matriz_Z = [0 Z_12 Z_13 ; Z_12 0 Z_23 ; Z_13 Z_12 0]; % Impedancia das linhas de
 matriz_Y = zeros(n,n);
 
 for i = 1:1:n
-    for j = 1:1:n
-        if(matriz_Z(i,j) ~= 0)
-            matriz_Y(i,j) = -(1 / matriz_Z(i,j));
+    for z = 1:1:n
+        if(matriz_Z(i,z) ~= 0)
+            matriz_Y(i,z) = -(1 / matriz_Z(i,z));
         end
         
-        if(j == n)
+        if(z == n)
             for(k = 1:1:n)
             if( k ~= i)
-                matriz_Y(i,i) = (matriz_Y(i,i) - matriz_Y(i,k));
+                matriz_Y(i,i) = (matriz_Y(i,i) + (-(matriz_Y(i,k))));
+            else
+                %matriz_Y(i,i) = (matriz_Y(i,i) + B(1));
             end
+                
             end
         end
         
