e1ff81ff3e3957e145ad79c88754ac2c0eaffb74
Thiago E. Kalid DESKTOP <thiagokalid@protonmail.com>
2020-04-29 09:59:48 -0300

    Adicionando livecript

--- flxuo_potencia_LT.m
+++ flxuo_potencia_LT.m
@@ -1,4 +1,4 @@
-function [fluxo_de_potencia_LT] = flxuo_potencia_LT(variaveis_NR , final_X , matriz_V, matriz_theta, Y)
+function [fluxo_de_potencia_LT] = flxuo_potencia_LT(variaveis_NR , final_X , matriz_V, matriz_theta, Z)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 tamanho = size(matriz_V , 2);
@@ -9,7 +9,7 @@ Y_G = zeros(tamanho , tamanho);
 fluxo_de_potencia_LT = zeros(tamanho , tamanho);
 for p = 1:1:tamanho
     for q = 1:1:tamanho
-        fluxo_de_potencia_LT(p , q) = conj(E(p)) * (E(p) - E(q)) * Y(p,q) + conj(E(p))* E(p) * (Y_G(p,q)/2);
+        fluxo_de_potencia_LT(p , q) = E(p) * (conj(E(p)) - conj(E(q)))/conj(Z(p,q));
     end
 end
 end
--- main.m
+++ main.m
@@ -7,7 +7,7 @@ clear
 clc
 % Input do usuário:
 % Número de barramentos
-numBarra = 5;
+numBarra = 3;
 
 %%
 % Constantes para facilitar a leitura:
@@ -27,51 +27,29 @@ matriz_YG = zeros(numBarra , 1);
 matriz_Z = zeros(numBarra,numBarra);
 
 % Impedância da LT entre os barramentos 1-2 ou 2-1 (em p.u.):
-matriz_Z(1,2) = 0.0200 + j*0.0600;
-matriz_Z(2,1) = 0.0200 + j*0.0600;
+matriz_Z(1,2) = 0.0047 + j*0.0474;
+matriz_Z(2,1) = 0.0047 + j*0.0474;
 
 % Impedância da LT entre os barramentos 1-3 ou 3-1(em p.u.):
-matriz_Z(1,3) = 0.0800 + j*0.2400;
-matriz_Z(3,1) = 0.0800 + j*0.2400;
+matriz_Z(1,3) = 0.0062 + j*0.0632;
+matriz_Z(3,1) = 0.0062 + j*0.0632;
 
 % Impedância da LT entre os barramentos 2-3 ou 3-2(em p.u.):
-matriz_Z(2,3) = 0.0600 + j*0.1800;
-matriz_Z(3,2) = 0.0600 + j*0.1800;
+matriz_Z(2,3) = 0.0047 + j*0.0474;
+matriz_Z(3,2) = 0.0047 + j*0.0474;
 
-% Impedância da LT entre os barramentos 2-4 ou 4-2(em p.u.):
-matriz_Z(2,4) = 0.0600 + j*0.1800;
-matriz_Z(4,2) = 0.0600 + j*0.1800;
-
-% Impedância da LT entre os barramentos 2-5 ou 5-2(em p.u.):
-matriz_Z(2,5) = 0.0400 + j*0.1200;
-matriz_Z(5,2) = 0.0400 + j*0.1200;
-
-% Impedância da LT entre os barramentos 3-4 ou 4-3(em p.u.):
-matriz_Z(3,4) = 0.0100 + j*0.0300;
-matriz_Z(4,3) = 0.0100 + j*0.0300;
-
-% Impedância da LT entre os barramentos 4-5 ou 5-4(em p.u.):
-matriz_Z(4,5) = 0.0800 + j*0.2400;
-matriz_Z(5,4) = 0.0800 + j*0.2400;
 
 
 
 
 % Admitância barramento-terra na barra 1, em p.u.:
-matriz_YG(1) = 0 + j*0.05500;
+matriz_YG(1) = 0 ;
 
 % Admitância barramento-terra na barra 2, em p.u.:
-matriz_YG(2) = 0 + j*0.08500;
+matriz_YG(2) = 0 ;
 
 % Admitância barramento-terra na barra 3, em p.u.:
-matriz_YG(3) = 0 + j*0.05500;
-
-% Admitância barramento-terra na barra 4, em p.u.:
-matriz_YG(4) = 0 + j*0.05500;
-
-% Admitância barramento-terra na barra 5, em p.u.:
-matriz_YG(5) = 0 + j*0.04000;
-
+matriz_YG(3) = 0 ;
 
 
 
@@ -86,36 +64,24 @@ matriz_Y = matriz_admitancia(matriz_Z, matriz_YG);
 % Se for barra tipo PQ, deve-se definir Potência ativa em p.u. e Potência reativa em p.u..
 
 % Base de potência para o cálculo em p.u.
-P_base = 100000e3;
+P_base = 1;
 
 % Barramento 1: SLACK
 Tipo_barra(1) = SLACK;
-matriz_V(1) = 1.06;
+matriz_V(1) = 1.0;
 matriz_theta(1) = 0;
 
 
 
 % Barramento 2: PQ
-Tipo_barra(2) = PQ;
-matriz_P(2) = (40e6  - 20e6)/ P_base;
-matriz_Q(2) = (30e6 - 10e6)/P_base;
+Tipo_barra(2) = PV;
+matriz_P(2) = 2.00;
+matriz_V(2) = 1.05;
 
 % Barramento 3: PQ
 Tipo_barra(3) = PQ;
-matriz_P(3) = (0e6 - 45e6)/P_base;
-matriz_Q(3) = (0e6 - 15e6)/P_base;
-
-% Barramento 4: PQ
-Tipo_barra(4) = PQ;
-matriz_P(4) = (0e6 - 40e6)/P_base;
-matriz_Q(4) = (0e6 - 5e6)/P_base;
-
-
-% Barramento 5: PQ
-Tipo_barra(5) = PQ;
-matriz_P(5) = (0e6 - 60e6)/P_base;
-matriz_Q(5) = (0e6 - 10e6)/P_base;
-
+matriz_P(3) = -5.00;
+matriz_Q(3) = -1.00;
 
 
 P_esp = vpa(matriz_P + matriz_Q * 1i);
@@ -149,7 +115,7 @@ printP_barras(P_das_barras);
 
 printX(variaveis_NR , final_X, matriz_V, matriz_theta);
 
-[P_nas_LTs] = flxuo_potencia_LT(variaveis_NR , final_X , matriz_V, matriz_theta, matriz_Y);
+[P_nas_LTs] = flxuo_potencia_LT(variaveis_NR , final_X , matriz_V, matriz_theta, matriz_Z);
 printP_LTs(P_nas_LTs);
 
 
